ea972cad3cd45d812df12d0a6b04e5550c25468d
Andrew Chilton <andychilton@gmail.com>
2019-08-23 10:16:21 +1200

    Handle errors from plugins correctly

--- ardash.js
+++ ardash.js
@@ -16,6 +16,7 @@ const moon = require('./lib/plugin/moon.js')
 const datetime = require('./lib/plugin/datetime.js')
 const anniversary = require('./lib/plugin/anniversary.js')
 const sun = require('./lib/plugin/sun.js')
+const error = require('./lib/plugin/error.js')
 
 // ----------------------------------------------------------------------------
 // setup
@@ -25,6 +26,7 @@ const plugin = {
   datetime,
   anniversary,
   sun,
+  error,
 }
 
 const client  = mqtt.connect()
@@ -49,40 +51,99 @@ Object.keys(config).forEach((title, pageNum) => {
   })
 })
 
-async function setup() {
+function setup() {
+  debug('setup()')
+
   const setups = pages.map(page => {
     return plugin[page.plugin].setup(page)
   })
 
   // call all plugin `setup()` functions
-  await Promise.all(setups).catch(err => {
+  return Promise.all(setups).catch(err => {
     console.warn(err)
     process.exit(2)
   })
 }
 
 function publish(title, plugin, channel, lines) {
-  fmt.msg(`${title} (${plugin}):`)
+  fmt.arrow(`${title} (${plugin})`)
   for(let i=0; i<lines.length; i++) {
-    fmt.li(lines[i])
+    fmt.li(lines[i], true)
   }
-  client.publish(`ardash/${channel}`, JSON.stringify(lines))
+  fmt.spacer()
+  const msg = {
+    title,
+    lines,
+  }
+  client.publish(`ardash/${channel}`, JSON.stringify(msg))
 }
 
-function go() {
-  fmt.title((new Date()).toISOString())
+function run() {
+  debug('run()')
+
+  fmt.line()
+  fmt.spacer()
+  fmt.arrow((new Date()).toISOString())
+  fmt.msg(`There are ${Object.keys(pages).length} sections.`, true)
+  fmt.spacer()
 
   const promises = pages.map((page, pageNum) => {
     return plugin[page.plugin].go(page).then(lines => {
       publish(page.title, page.plugin, pageNum, lines)
+    }).catch(err => {
+      publish(page.title, page.plugin, pageNum, [ 'Err: check Ardash log' ])
+      console.warn(err)
+      console.warn()
     })
   })
 
-  Promise.all(promises).then(() => {
-    fmt.msg('')
+  return Promise.all(promises).then(() => {
+    fmt.arrow('Finished')
+    fmt.spacer()
   }).catch(err => {
+    console.warn('Program Error')
+    console.warn()
+    console.warn("When we call each plugin, we catch at source and shouldn't")
+    console.warn("propogate upwards, i.e. here. This is probably some other")
+    console.warn("error with the program we should fix. Please help us by")
+    console.warn("raising an issue on https://github.com/chilts/ardash - thanks!")
+    console.warn()
     console.warn(err)
+    console.warn()
   })
+}
+
+function start() {
+  return setInterval(run, ms(process.env.NODE_ENV === 'production' ? '1 min' : '10s'))
+}
+
+function shutdown(signal) {
+  return err => {
+    fmt.line()
+    fmt.spacer()
+    fmt.arrow(`Received ${signal}`)
+    if (err) {
+      console.error(err.stack || err)
+    }
+
+    fmt.indent('Shutting down each plugin:')
+    const shutdowns = pages.map(page => {
+      fmt.li(page.title, true)
+      return plugin[page.plugin].shutdown(page)
+    })
+
+    // if any plugin doesn't shutdown within 10s, then we'll exit with force
+    setTimeout(() => {
+      fmt.arrow(`Exiting forcefully after 10s since ${signal}`)
+      process.exit(err ? 1 : 0)
+    }, ms('5 secs')).unref()
+
+    return Promise.all(shutdowns).catch(err => {
+      console.warn(err)
+      process.exit(2)
+    })
+
+  }
 }
 
 // ----------------------------------------------------------------------------
@@ -95,16 +156,24 @@ fmt.msg("     / _ \\ | '__/ _` |/ _` / __| '_ \\ ")
 fmt.msg('    / ___ \\| | | (_| | (_| \\__ \\ | | |')
 fmt.msg('   /_/   \\_\\_|  \\__,_|\\__,_|___/_| |_|')
 fmt.msg('')
+fmt.spacer()
 
-;(async () => {
+let intervalId = null
 
-  debug('Calling setup ...')
-  await setup()
+setup()
+  .then(run)
+  .then(start)
+  .then(id => {
+    intervalId = id
+  })
+  .catch(err => {
+    console.warn(err)
+  })
+;
 
-  debug('Doing interval ...')
-  setInterval(go, ms(process.env.NODE_ENV === 'production' ? '1 min' : '10s'))
-  go()
-
-})()
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'))
 
 // ----------------------------------------------------------------------------
